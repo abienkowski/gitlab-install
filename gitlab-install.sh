f6e75c0942f2c7080ec5952cd089e7f058595b26
Adrian Bienkowski <abienkow@gitlab-ldap-integration.lab.ethoca.com>
2017-05-20 12:29:46 +0000

    Updated installation steps with more options.

--- gitlab-install.sh
+++ gitlab-install.sh
@@ -7,6 +7,15 @@
 #	ldap-install
 #	keycloack-install
 # -- --
+# install options 
+# -- --
+RUN_UPDATE_AND_UPGRADE=false
+INSTALL_GO=false
+INSTALL_DEPS=false
+INSTALL_SYSTEM_USER=false
+INSTALL_DB_POSTGRES=false
+INSTALL_DB_REDIS=false
+INSTALL_GITLAB=true
 # -- --
 GITLAB_VERSION=8.3.2
 RUBY_VERSION=2.3
@@ -30,23 +39,141 @@ GITLAB_DATA_DIR="${GITLAB_HOME}/data"
 GITLAB_BUILD_DIR="${GITLAB_CACHE_DIR}/build"
 GITLAB_RUNTIME_DIR="${GITLAB_CACHE_DIR}/runtime"
 # -- --
+# If you want to use Kerberos for user authentication, then install libkrb5-dev:
+# -- --
+AUTH_KERBEROS=false
+# -- --
+# run update and upgrade
+# -- --
+if ( $RUN_UPDATE_AND_UPGRADE ); then
+  apt-get update && apt-get upgrade -y
+fi
+# -- --
 # install all dependencies
 # -- --
-sudo apt-get install curl openssh-server ca-certificates postfix
+# Install the required packages (needed to compile Ruby and native extensions to Ruby gems)
+# -- --
+DEPENDENCIES="build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev curl openssh-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev logrotate python-docutils pkg-config cmake nodejs"
+if ( $AUTH_KERBEROS ); then
+  DEPENDENCIES="$DEPENDENCIES libkrb5-dev"
+  echo "Adding libkrb5-dev for Kerberos support..."
+fi
+# -- --
+# install all dependencies
+# -- --
+if ( $INSTALL_DEPS ); then
+  apt-get install -y $DEPENDENCIES
+fi
+# -- --
+# install go
+# -- --
+if ( $INSTALL_GO ); then
+  # -- --
+  # lookup the checksum value for specific release here: https://golang.org/dl/
+  # -- --
+  curl -O --progress https://storage.googleapis.com/golang/go${GOLANG_VERSION}.linux-amd64.tar.gz
+  echo "cdde5e08530c0579255d6153b08fdb3b8e47caabbe717bc7bcd7561275a87aeb go${GOLANG_VERSION}.linux-amd64.tar.gz" | sha256sum -c - && \
+    tar -C /usr/local -xzf go${GOLANG_VERSION}.linux-amd64.tar.gz
+  ln -sf /usr/local/go/bin/{go,godoc,gofmt} /usr/local/bin/
+  rm go${GOLANG_VERSION}.linux-amd64.tar.gz
+fi
+# -- --
+# add system user
+# -- --
+if ( $INSTALL_SYSTEM_USER ); then
+  echo -n "Adding system user ${GITLAB_USER}..."
+  adduser --disabled-login --gecos 'GitLab' $GITLAB_USER
+  echo "done"
+fi
+# -- --
+# install postgres
+# -- --
+if ( $INSTALL_DB_POSTGRES ); then
+  # Install the database packages
+  apt-get install -y postgresql postgresql-client libpq-dev
+  # Login to PostgreSQL
+  sudo -u postgres bash -c "psql -c \"CREATE USER git CREATEDB;\""
+  sudo -u postgres bash -c "psql -c \"CREATE DATABASE gitlabhq_production OWNER git;\""
+  echo -n "Looking for the created database ... found -> "
+  sudo -u git -H bash -c "psql -lqt | cut -d \| -f 1 | grep -w gitlabhq_production"
+fi
+# -- --
+# install redis
+# -- --
+if ( $INSTALL_DB_REDIS ); then
+  # Add git to the redis group
+  usermod -aG gitlab-redis $GITLAB_USER
+fi
+# -- --
+# clone gitlab
+# -- --
+if ( $CLONE_GITLAB_VERSION ); then
+  cd $GITLAB_USER_HOME
+  # -- TODO: change 8-3-stable to be driven from the configuraiton above
+  git clone https://gitlab.com/gitlab-org/gitlab-ce.git -b 8-3-stable gitlab
+fi
+# -- --
+# install gitlab
+# -- --
+if ( $INSTALL_GITLAB ); then
+  # gitlab home directory
+# Go to GitLab installation folder
+cd /home/git/gitlab
 
-#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E1DD270288B4E6030699E45FA1715D88E1DF1F24 \
-# && echo "deb http://ppa.launchpad.net/git-core/ppa/ubuntu trusty main" >> /etc/apt/sources.list \
-# && apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 80F70E11F0F0D5F10CB20E62F5DA5F09C3173AA6 \
-# && echo "deb http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu trusty main" >> /etc/apt/sources.list \
-# && apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 8B3981E7A6852F782CC4951600A6F0A3C300EE8C \
-# && echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu trusty main" >> /etc/apt/sources.list \
-# && wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add - \
-# && echo 'deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main' > /etc/apt/sources.list.d/pgdg.list \
-# && wget --quiet -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add - \
-# && echo 'deb https://deb.nodesource.com/node_7.x trusty main' > /etc/apt/sources.list.d/nodesource.list \
-# && wget --quiet -O - https://dl.yarnpkg.com/debian/pubkey.gpg  | apt-key add - \
-# && echo 'deb https://dl.yarnpkg.com/debian/ stable main' > /etc/apt/sources.list.d/yarn.list \
-# && apt-get update \
+# Copy the example GitLab config
+sudo -u git -H cp config/gitlab.yml.example config/gitlab.yml
+
+# Update GitLab config file, follow the directions at top of file
+sudo -u git -H editor config/gitlab.yml
+
+# Copy the example secrets file
+sudo -u git -H cp config/secrets.yml.example config/secrets.yml
+sudo -u git -H chmod 0600 config/secrets.yml
+
+# Make sure GitLab can write to the log/ and tmp/ directories
+sudo chown -R git log/
+sudo chown -R git tmp/
+sudo chmod -R u+rwX,go-w log/
+sudo chmod -R u+rwX tmp/
+
+# Make sure GitLab can write to the tmp/pids/ and tmp/sockets/ directories
+sudo chmod -R u+rwX tmp/pids/
+sudo chmod -R u+rwX tmp/sockets/
+
+# Make sure GitLab can write to the public/uploads/ directory
+sudo chmod -R u+rwX  public/uploads
+
+# Change the permissions of the directory where CI build traces are stored
+sudo chmod -R u+rwX builds/
+
+# Change the permissions of the directory where CI artifacts are stored
+sudo chmod -R u+rwX shared/artifacts/
+
+# Copy the example Unicorn config
+sudo -u git -H cp config/unicorn.rb.example config/unicorn.rb
+
+# Find number of cores
+nproc
+
+# Enable cluster mode if you expect to have a high load instance
+# Set the number of workers to at least the number of cores
+# Ex. change amount of workers to 3 for 2GB RAM server
+sudo -u git -H editor config/unicorn.rb
+
+# Copy the example Rack attack config
+sudo -u git -H cp config/initializers/rack_attack.rb.example config/initializers/rack_attack.rb
+
+# Configure Git global settings for git user, used when editing via web editor
+sudo -u git -H git config --global core.autocrlf input
+
+# Configure Redis connection settings
+sudo -u git -H cp config/resque.yml.example config/resque.yml
+
+# Change the Redis socket path if you are not using the default Debian / Ubuntu configuration
+sudo -u git -H editor config/resque.yml
+  
+fi
+
 # && apt-get install -y logrotate locales curl \
 #      nginx openssh-server postgresql-client redis-tools \
 #      git-core ruby${RUBY_VERSION} python2.7 python-docutils nodejs yarn gettext-base \
